6eb1bca6ca2f6f4c6c6c68349b2c8703884e8205
Kathuthu <arthur.vettorazzo@gmail.com>
2026-03-20 20:50:21 -0300

    aula2-variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02-variaveis-operadores.py
@@ -0,0 +1,37 @@
+print("Vai Corinthians")
+
+print(7 + 4)
+print("7 + 4")
+print("7"+"4")
+'''
+comentarios de varias linhas
+'''
+#variaveis
+nome="Arthur" #String
+idade=19 #int
+peso=80.1 #float
+print(nome,idade,peso)
+print(f"oi,{nome}")
+
+nome = input("Digite sue nome:")
+print(nome)
+idade = int(input("digite sua idade"))
+print(nome, idade)
+print(idade + 1)
+
+'''
+desafios
+'''
+#1
+nome = input("Digite seu nome:")
+print("boas-vindas " + nome)
+
+#2
+nasc = date =input("Digite sua data de nascimento: ")
+print("voce nasceu em: " + nasc)
+
+#3
+n1 = int = input("digite o primeiro numero para a soma: ")
+n2 = int = input("digite o segundo numero para a soma:  ")
+
+
