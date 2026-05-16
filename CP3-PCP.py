6c9f2fb3815e860cdebdc002c2dd1032c1757622
Vettorazzo <arthur.vettorazzo@gmail.com>
2026-05-15 21:36:40 -0300

    CP3 de PCP finalizada
    Added functionality to track the room with the highest number of critical temperatures.

--- CP3-PCP.py
+++ CP3-PCP.py
@@ -8,21 +8,35 @@ temperaturas = [
 i = 0
 media = 0
 criticos = 0
+sala_critica = 0
+numero_sala_critica = 1
 for sala in temperaturas:
-    print(sala)
+
 
     for temperatura in sala:
-        print(temperatura)
         if temperatura >= 33:
             criticos +=1
 
-    print(f"sala {i+1}")
+    print(f"Sala: {i+1}")
+
 
     media = sum(sala) / len(sala)
-    print(media)
-    print(criticos)
+    print(f"Média de temperatura: {media}")
+    print(f"Número de temperaturas criticas: {criticos}")
+
+
+    if criticos > sala_critica:
+        sala_critica = criticos
+        numero_sala_critica = i+1
+
 
     i+=1
     criticos = 0
 
+
     print()
+
+
+
+
+print(f"A sala com maior quantidade de temperatura é: {numero_sala_critica}")
